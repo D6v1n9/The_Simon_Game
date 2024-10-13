ec09e0bdf112821999d9ef52faa65df0a80cb86f
D6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2024-10-14 00:45:36 +0530

    Added HTML and CSS code

---
+++ game.js
@@ -0,0 +1,29 @@
+$(document).on("keypress", function(event) {
+    if(event.key === "A") {
+        startGame() ;
+    }
+})
+//Math.floor(Math.random()*5);
+function startGame() {
+    let arr = [] ;
+    arr.push(Math.floor(Math.random()*4));
+    check(arr) ;
+}
+
+function check(arr) {
+    $("button").on("click",function(event) {
+        switch (event) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+    })
+    while(true) {
+        for(let i=0;i<arr.size();i++) {
+
+        }
+    }
+}

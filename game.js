4afeed15d3384ab1c051b51f7fe25e31b2b61eaf
D6v1n9 <132968913+D6v1n9@users.noreply.github.com>
2024-10-17 21:28:25 +0530

    Update game.js and completed the Project

--- game.js
+++ game.js
@@ -1,32 +1,80 @@
 let buttonColours = ["green","red","yellow","blue"] ;
 let gamePattern = [] ; // store the computer value 
 let userClickedPattern = [] ; // array which will store the user value that it clicked
+
+//to check whether the game is started or not need to create an variable bool
+let started = false ;
+
+// to change the text of h1 you need another variable 
+let level = 0 ;
+
+$(document).keydown(function(event) {
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true ;
+    }
+})
+
+// getting user input by clicking on the buttons present
+
+$(".btn").click(function(event) {
+    let userChosenColour = event.target.id; // or this.id  or $(this).attr("id") or event.target.getAttribute("id")
+    userClickedPattern.push(userChosenColour) ;
+    //console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1) ;
+})
+
+// will compare answer with random genrated (gamePattern && userClickedPattern) compare
+
+function checkAnswer(currentLevel) {
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if(userClickedPattern.length === gamePattern.length) {
+            userClickedPattern = [] ;
+            setTimeout(function(){
+                nextSequence();
+            },1000) ;
+        }
+        
+    }
+    else {
+        playSound("wrong");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        },200);
+
+        startOver();
+    }
+}
+// restart the game by changing the values to intial
+function startOver() {
+    level = 0 ;
+    gamePattern = [] ;
+    started = false ;
+}
+
+
 // getting data using random number by system 
-
-startGame();
-
 function nextSequence() {
+    //every time next sequence is called it will increase the level and change the h1
+    level++ ;
+    $("#level-title").text("Level " + level);
     let randomNumber = Math.floor(Math.random()*4);
     let randomChosenColour = buttonColours[randomNumber] ;
     gamePattern.push(randomChosenColour) ;
-    animateButton(randomChosenColour) ;
+    animateButton(randomChosenColour) ;    
 }
 // setting animation on the button 
 function animateButton(colour) {
-    var colourSound = new Audio("./sounds/"+colour+".mp3");
+    let colourSound = new Audio("./sounds/"+colour+".mp3");
     colourSound.play();
     $("#"+colour).animate({ opacity:0.1 },100).animate({opacity:1},100);
 };
-// getting user input using handler function 
-function handler() {
-    $(".btn").click(function(event) {
-        let userChosenColour = event.target.id; // or this.id  or $(this).attr("id") or event.target.getAttribute("id")
-        userClickedPattern.push(userChosenColour) ;
-        // console.log(userClickedPattern) ;
-        playSound(userChosenColour);
-        animatePress(userChosenColour);
-    })
-}
+
 // on click we want sound right ? so the below function is for that purpose
 function playSound(name) {
     var colourSound = new Audio("./sounds/"+name+".mp3");
@@ -40,13 +88,3 @@ function animatePress(name) {
     },100);
 }
 
-//Getting A key press to start the game 
-function startGame() {
-    $(document).keydown(function(event) {
-        if(event.key == "a") {
-            nextSequence();
-            let level = 1 ;
-        
-        }
-    })
-}
